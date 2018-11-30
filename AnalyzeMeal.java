38f65f542818344986d549cd58c76400382b6598
dberman4 <36210694+dberman4@users.noreply.github.com>
2018-11-30 16:36:09 -0600

    Create AnalyzeMeal.java

---
+++ AnalyzeMeal.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class AnalyzeMeal {
+    static Stage window;
+    public static void display(){
+        window = new Stage();
+        window.setTitle("Analyze Meal");
+        window.setMinWidth(600);
+
+
+        window.show();
+
+    }
+}
