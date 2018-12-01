6dd88c231f4a9f017094f2c812b8dc664fdc6490
tannerbart <43884513+tannerbart@users.noreply.github.com>
2018-11-30 18:50:55 -0600

    added comments

--- FilterError.java
+++ FilterError.java
@@ -5,9 +5,20 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
-
+/**
+ * Filename:   FilterError.java
+ * Project:    Food Query
+ * Authors:    Amanda Sarsha, Tanner Bart, Xuefeng Xu, David Berman
+ * 
+ * Use this class for displaying the given error with filtering
+ */
 public class FilterError {
-    static Stage window;
+    static Stage window;    //stage to be displayed
+    /**
+     * Display the error message with filter
+     * 
+     * @param message   String of which error occured
+     */
     public static void display(String message){
         window = new Stage();
         window.setTitle("Error");
