a710ad4bf8d340bab8bb73ea7c727ac7ca1dfba7
tannerbart <43884513+tannerbart@users.noreply.github.com>
2018-11-30 18:54:19 -0600

    Added comments

--- LoadFood.java
+++ LoadFood.java
@@ -5,12 +5,20 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
-
+/**
+ * Filename:   LoadFood.java
+ * Project:    Food Query
+ * Authors:    Amanda Sarsha, Tanner Bart, Xuefeng Xu, David Berman
+ * 
+ * Use this class for loading a given file to the program
+ */
 public class LoadFood {
     static Stage window;
-
+    /**
+     * Displays the window to allow the user to load a food file
+     */
     public static void display(){
-        window = new Stage();
+        window = new Stage();   //stage to be displayed
         window.setTitle("Load");
         window.setMinWidth(300);
 
@@ -22,9 +30,10 @@ public class LoadFood {
         grid.setVgap(8);
         grid.setHgap(10);
 
-
+        //Submit button to allow for the user to submit the given file
         Button btn = new Button("Submit");
         btn.setOnAction(event -> {
+            //need to implement actual importing of file
             window.close();
         });
         grid.add(loadPath, 0,0);
@@ -36,7 +45,7 @@ public class LoadFood {
 
 
 
-
+        //show the window
         window.show();
 
     }
