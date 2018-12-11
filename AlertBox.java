61f0ff0ac5933076f3bed42f82d94dd9bbd29a30
Xuefeng Xu <40620590+FrankXuefengXu@users.noreply.github.com>
2018-12-10 20:40:10 -0600

    Create AlertBox.java

---
+++ AlertBox.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class AlertBox {
+
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Cool!");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+        grid.add(layout, 0, 0);
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(grid);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
