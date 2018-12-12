0001eedd79f438449da859bb761eb19d95800d52
Xuefeng Xu <40620590+FrankXuefengXu@users.noreply.github.com>
2018-12-12 12:23:11 -0600

    Add Comments

--- TableItem.java
+++ TableItem.java
@@ -3,15 +3,33 @@ package application;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Filename:   application.TableItem.java
+ * Project:    Food Query
+ * Authors:    Amanda Sarsha, Tanner Bart, Xuefeng Xu, David Berman
+ *
+ * This class constructs each item stored in the GUI table view
+ */
 public class TableItem{
+    // column names
     private SimpleStringProperty name;
     private SimpleDoubleProperty value;
 
+    /**
+     * Constructor
+     *
+     * @param name nutrient name
+     * @param value nutrient value
+     */
     TableItem(String name, Double value){
         this.name = new SimpleStringProperty(name);
         this.value = new SimpleDoubleProperty(value);
     }
 
+    /**
+     * Getters and Setters
+     * @return name of the nutrient
+     */
     public String getName() {
         return name.get();
     }
